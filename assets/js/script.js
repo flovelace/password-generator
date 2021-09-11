d9cee9587d7659a73ceea518e77e93c724a30e2e
fairfax <lovelace.af@gmail.com>
2021-09-11 00:18:35 -0400

    wrote the function to generate the password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+//  var password = generatePassword();
+//  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//  passwordText.value = password;
 
-}
+//}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+
+var generateBtn = document.getElementById("generate");
+
+generateBtn.addEventListener("click", () => {
+  var passwordLength = 16;
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_";
+  var password = "";
+
+  for (i = 0;i < passwordLength;i++){
+      password = password + characters.charAt(Math.floor(Math.random() * Math.floor(characters.length - 1)));
+  }
+
+  document.getElementById("password").value = password;
+});
